fcbf531f684f9f7900208cc27f37cb6653f8cc43
Kiu Ai <aij914@gmail.com>
2021-01-16 20:21:42 +0800

    setup project.

---
+++ mobile-ios-grid-image-async-loading/mobile_ios_grid_image_async_loadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_ios_grid_image_async_loadingApp.swift
+//  mobile-ios-grid-image-async-loading
+//
+//  Created by Kiu Ai on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct mobile_ios_grid_image_async_loadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
