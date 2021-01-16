bf384aaa38baa3f07ce6254dbbf4da7cadcab5b3
Kiu Ai <aij914@gmail.com>
2021-01-16 21:14:29 +0800

    layout basic Grid column

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mobile-ios-grid-image-async-loading
 Swift UI 2.0 Project to display grid view of images with async data loading
+
+
+
+## JSON Feed Generator from Apple
+
+https://rss.itunes.apple.com/en-us
+
+
+
--- mobile-ios-grid-image-async-loading/ContentView.swift
+++ mobile-ios-grid-image-async-loading/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200))
+                ], spacing:12, content: {
+                    ForEach(0..<20, id: \.self) { num in
+                        VStack (alignment: .leading, spacing: 0, content: {
+                            Spacer().frame(width: 100, height: 100, alignment: .center).background(Color.blue)
+                            Text("App Title").font(.system(size: 10, weight:.semibold))
+                            Text("Relesase Date").font(.system(size: 9, weight:.regular))
+                            Text("Copyright").font(.system(size: 9, weight:.regular)).foregroundColor(.gray)
+                        }).padding().background(Color.red)
+                    }
+
+                }).padding(.horizontal, 12)
+                
+            }.navigationTitle("Dummy Title")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
+ 
