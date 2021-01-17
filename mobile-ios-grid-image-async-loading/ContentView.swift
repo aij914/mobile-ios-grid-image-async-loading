8d81f948b0234e76dca9b789e181c60992fd5f87
Kiu Ai <aij914@gmail.com>
2021-01-17 12:58:08 +0800

    use kingfisher to download image.

--- mobile-ios-grid-image-async-loading/ContentView.swift
+++ mobile-ios-grid-image-async-loading/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct RSS: Decodable {
     let feed: Feed
@@ -20,19 +21,21 @@ struct Result:Decodable, Hashable {
 }
 
 class GridViewModel: ObservableObject {
-    @Published var items = 0..<10
     @Published var results = [Result]()
 
     init() {
         guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/cn/ios-apps/top-free/all/100/explicit.json") else {
             return
         }
+        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             do {
                 let rss = try JSONDecoder().decode(RSS.self, from: data)
                     print(rss)
-                self.results = rss.feed.results
+                DispatchQueue.main.async {
+                    self.results = rss.feed.results
+                }
             } catch {
                 print("Failed to decode: \(error)")
             }
@@ -48,22 +51,16 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: [
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
-                    GridItem(.flexible(minimum: 100, maximum: 200), alignment: .top)
-                ], spacing:12, content: {
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top),
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top),
+                    GridItem(.flexible(minimum: 50, maximum: 200), spacing: 16, alignment: .top)
+                ], spacing:16, content: {
                     ForEach(vm.results, id: \.self) { result in
-                        VStack (alignment: .leading, spacing: 0, content: {
-                            Spacer().frame(width: 100, height: 100, alignment: .center).background(Color.blue)
-                            Text(result.name).font(.system(size: 10, weight:.semibold))
-                            Text(result.releaseDate).font(.system(size: 9, weight:.regular))
-                            Text(result.copyright).font(.system(size: 9, weight:.regular)).foregroundColor(.gray)
-                        }).padding(.horizontal)
+                        AppView(result: result)
                     }
-
                 }).padding(.horizontal, 12)
                 
-            }.navigationTitle("Dummy Title")
+            }.navigationTitle("Grid Layout Title")
         }
     }
 }
@@ -76,3 +73,19 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
  
+struct AppView: View {
+    
+    let result: Result
+    
+    var body: some View {
+        VStack (alignment: .leading, spacing: 4, content: {
+            
+            KFImage(URL(string: result.artworkUrl100)).resizable().scaledToFit().cornerRadius(22)
+            Text(result.name).font(.system(size: 10, weight:.semibold)).padding(.top, 4)
+            Text(result.releaseDate).font(.system(size: 9, weight:.regular))
+            Text(result.copyright).font(.system(size: 9, weight:.regular)).foregroundColor(.gray)
+            
+            Spacer()
+        })
+    }
+}
